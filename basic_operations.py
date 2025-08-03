47e7b56dd51d6058d758bb0f37e7bee38828c0d2
nahashon69 <nahashonm69@gmail.com>
2025-08-03 16:38:17 +0300

    fourth review and correction

---
+++ basic_operations.py
@@ -0,0 +1,13 @@
+number1=10
+number2=5
+
+sum_results=number1+number2
+difference_results=number1-number2
+product_results=number1*number2
+
+
+print(f"sum of {number1} and {number2} is {sum_results}")
+
+print(f"difference of {number2} and {number2} is {difference_results} ")
+          
+print (f" product of {number1} and {number2} is{product_results}")
---
+++ simple_interest.py
@@ -0,0 +1,7 @@
+Principal=1000
+rate = 0.05
+time = 3
+
+interest = Principal * rate * time
+
+print(f"print The simple interest is :{interest}")
